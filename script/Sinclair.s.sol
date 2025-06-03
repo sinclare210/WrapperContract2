2c30c1f19ab0fcff293142a3b200aa454e4154fd
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-06-03 09:39:04 +0100

    On it

--- script/Sinclair.s.sol
+++ script/Sinclair.s.sol
@@ -2,7 +2,16 @@
 pragma solidity ^0.8.28;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Sinclair} from "../src/Sinclair.sol";
 
 contract SinclairScript is Script {
+    Sinclair public sinclair;
+
+    function run() external{
+        vm.startBroadcast();
+        sinclair = new Sinclair(0x0400a49dE8E485f037f8244EA92c8d11C025C1c5);
+        vm.stopBroadcast();
+    }
+    
     
 }
--- src/Sinclair.sol
+++ src/Sinclair.sol
@@ -6,7 +6,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SINCLAIR is ERC20, Ownable, ERC20Permit {
+contract Sinclair is ERC20, Ownable, ERC20Permit {
     constructor(address initialOwner)
         ERC20("SINCLAIR", "SIN")
         Ownable(initialOwner)
