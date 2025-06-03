fc16eccd2560a623f1d3a93e86863a10e40a6b64
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-06-03 10:19:07 +0100

    On it

--- test/WrapperContract.t.sol
+++ test/WrapperContract.t.sol
@@ -2,7 +2,23 @@
 pragma solidity ^0.8.28;
 
 import {Test, console} from "forge-std/Test.sol";
+import {Sinclair} from "../src/Sinclair.sol";
+import {WrapperContract} from "../src/WrapperContract.sol";
 
 contract WrapperContractTest is Test {
-    
+    Sinclair public sinclair;
+    WrapperContract public wrapperContract;
+
+    address sinc = address(0x1);
+
+    function setUp () external {
+        sinclair = new Sinclair(sinc);
+        wrapperContract = new WrapperContract(address(sinclair), WrapperContract.AssetType.TOKEN);
+
+        vm.startPrank(sinc);
+        sinclair.approve(address(wrapperContract), type(uint256).max);
+        vm.stopPrank();
+    }
+
+
 }
